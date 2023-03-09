06dfd5efcf7be247bdfe62d82e9fb79c80a15f29
Phil <PhilboBaggins@users.noreply.github.com>
2023-03-10 01:33:29 +1100

    Format code

--- ExampleCode/C with Pico SDK/PicoKeysExample.c
+++ ExampleCode/C with Pico SDK/PicoKeysExample.c
@@ -21,10 +21,11 @@
  * - 1000 ms : device mounted
  * - 2500 ms : device is suspended
  */
-enum  {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
+enum
+{
+    BLINK_NOT_MOUNTED = 250,
+    BLINK_MOUNTED = 1000,
+    BLINK_SUSPENDED = 2500,
 };
 
 static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
@@ -72,13 +73,13 @@ int main(void)
 // Invoked when device is mounted
 void tud_mount_cb(void)
 {
-  blink_interval_ms = BLINK_MOUNTED;
+    blink_interval_ms = BLINK_MOUNTED;
 }
 
 // Invoked when device is unmounted
 void tud_umount_cb(void)
 {
-  blink_interval_ms = BLINK_NOT_MOUNTED;
+    blink_interval_ms = BLINK_NOT_MOUNTED;
 }
 
 // Invoked when usb bus is suspended
@@ -86,14 +87,14 @@ void tud_umount_cb(void)
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
 void tud_suspend_cb(bool remote_wakeup_en)
 {
-  (void) remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
+    (void)remote_wakeup_en;
+    blink_interval_ms = BLINK_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
 void tud_resume_cb(void)
 {
-  blink_interval_ms = BLINK_MOUNTED;
+    blink_interval_ms = BLINK_MOUNTED;
 }
 
 //--------------------------------------------------------------------+
@@ -127,12 +128,12 @@ void hid_task(void)
     const uint32_t interval_ms = 10;
     static uint32_t start_ms = 0;
 
-    if ( board_millis() - start_ms < interval_ms)
+    if (board_millis() - start_ms < interval_ms)
         return; // not enough time
     start_ms += interval_ms;
 
-    static uint8_t prevSentKeycodes[6] = { 0 };
-    uint8_t keycodes[6] = { 0 };
+    static uint8_t prevSentKeycodes[6] = {0};
+    uint8_t keycodes[6] = {0};
 
     // Check each button
     for (int buttonIdx = 0; buttonIdx < NUM_BUTTONS; buttonIdx++)
@@ -141,18 +142,18 @@ void hid_task(void)
 
         switch (state)
         {
-            case BUTTON_NOT_PRESSED:
-                break;
-            case BUTTON_PRESSED:
-                addKey(keycodes, BUTTON_KEY_MAP[buttonIdx]);
-                break;
-            case BUTTON_HELD_PRESSED:
-                addKey(keycodes, BUTTON_KEY_MAP[buttonIdx]);
-                break;
-            case BUTTON_RELEASED:
-                break;
-            default:
-                break;
+        case BUTTON_NOT_PRESSED:
+            break;
+        case BUTTON_PRESSED:
+            addKey(keycodes, BUTTON_KEY_MAP[buttonIdx]);
+            break;
+        case BUTTON_HELD_PRESSED:
+            addKey(keycodes, BUTTON_KEY_MAP[buttonIdx]);
+            break;
+        case BUTTON_RELEASED:
+            break;
+        default:
+            break;
         }
     }
 
@@ -176,55 +177,57 @@ void hid_task(void)
 // Invoked when sent REPORT successfully to host
 // Application can use this to send the next report
 // Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len)
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint8_t len)
 {
-  (void) instance;
-  (void) len;
+    (void)instance;
+    (void)len;
 }
 
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
 // Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
 {
-  // TODO not Implemented
-  (void) instance;
-  (void) report_id;
-  (void) report_type;
-  (void) buffer;
-  (void) reqlen;
+    // TODO not Implemented
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
 
-  return 0;
+    return 0;
 }
 
 // Invoked when received SET_REPORT control request or received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
 {
-  (void) instance;
+    (void)instance;
 
-  if (report_type == HID_REPORT_TYPE_OUTPUT)
-  {
-    // Set keyboard LED e.g Capslock, Numlock etc...
-    if (report_id == REPORT_ID_KEYBOARD)
+    if (report_type == HID_REPORT_TYPE_OUTPUT)
     {
-      // bufsize should be (at least) 1
-      if ( bufsize < 1 ) return;
+        // Set keyboard LED e.g Capslock, Numlock etc...
+        if (report_id == REPORT_ID_KEYBOARD)
+        {
+            // bufsize should be (at least) 1
+            if (bufsize < 1)
+                return;
 
-      uint8_t const kbd_leds = buffer[0];
+            uint8_t const kbd_leds = buffer[0];
 
-      if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
-      {
-        // Capslock On: disable blink, turn led on
-        blink_interval_ms = 0;
-        board_led_write(true);
-      }else
-      {
-        // Caplocks Off: back to normal blink
-        board_led_write(false);
-        blink_interval_ms = BLINK_MOUNTED;
-      }
+            if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
+            {
+                // Capslock On: disable blink, turn led on
+                blink_interval_ms = 0;
+                board_led_write(true);
+            }
+            else
+            {
+                // Caplocks Off: back to normal blink
+                board_led_write(false);
+                blink_interval_ms = BLINK_MOUNTED;
+            }
+        }
     }
-  }
 }
 
 //--------------------------------------------------------------------+
@@ -232,16 +235,18 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 //--------------------------------------------------------------------+
 void led_blinking_task(void)
 {
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
+    static uint32_t start_ms = 0;
+    static bool led_state = false;
 
-  // blink is disabled
-  if (!blink_interval_ms) return;
+    // blink is disabled
+    if (!blink_interval_ms)
+        return;
 
-  // Blink every interval ms
-  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
-  start_ms += blink_interval_ms;
+    // Blink every interval ms
+    if (board_millis() - start_ms < blink_interval_ms)
+        return; // not enough time
+    start_ms += blink_interval_ms;
 
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
 }
