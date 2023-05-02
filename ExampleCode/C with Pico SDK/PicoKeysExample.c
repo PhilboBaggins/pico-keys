9a67955c0ede3de26753cb28f41dbdaacaaaf4fb
Phil <PhilboBaggins@users.noreply.github.com>
2023-05-02 20:16:58 +1000

    Remove LED blinking from example code because it really has nothing to do with this board

--- ExampleCode/C with Pico SDK/PicoKeysExample.c
+++ ExampleCode/C with Pico SDK/PicoKeysExample.c
@@ -11,24 +11,6 @@
 #include "buttons.h"
 #include "usb_descriptors.h"
 
-//--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
-//--------------------------------------------------------------------+
-
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum
-{
-    BLINK_NOT_MOUNTED = 250,
-    BLINK_MOUNTED = 1000,
-    BLINK_SUSPENDED = 2500,
-};
-
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
 // Map buttons to key codes - These will be send when the button is pressed
 uint8_t BUTTON_KEY_MAP[NUM_BUTTONS] =
 {
@@ -41,7 +23,6 @@ uint8_t BUTTON_KEY_MAP[NUM_BUTTONS] =
 #endif
 };
 
-void led_blinking_task(void);
 void hid_task(void);
 
 //--------------------------------------------------------------------+
@@ -56,7 +37,6 @@ int main(void)
     while (true)
     {
         tud_task(); // tinyusb device task
-        led_blinking_task();
         hid_task();
     }
 
@@ -70,13 +50,13 @@ int main(void)
 // Invoked when device is mounted
 void tud_mount_cb(void)
 {
-    blink_interval_ms = BLINK_MOUNTED;
+
 }
 
 // Invoked when device is unmounted
 void tud_umount_cb(void)
 {
-    blink_interval_ms = BLINK_NOT_MOUNTED;
+
 }
 
 // Invoked when usb bus is suspended
@@ -85,13 +65,12 @@ void tud_umount_cb(void)
 void tud_suspend_cb(bool remote_wakeup_en)
 {
     (void)remote_wakeup_en;
-    blink_interval_ms = BLINK_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
 void tud_resume_cb(void)
 {
-    blink_interval_ms = BLINK_MOUNTED;
+
 }
 
 //--------------------------------------------------------------------+
@@ -198,51 +177,8 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
 {
     (void)instance;
-
-    if (report_type == HID_REPORT_TYPE_OUTPUT)
-    {
-        // Set keyboard LED e.g Capslock, Numlock etc...
-        if (report_id == REPORT_ID_KEYBOARD)
-        {
-            // bufsize should be (at least) 1
-            if (bufsize < 1)
-                return;
-
-            uint8_t const kbd_leds = buffer[0];
-
-            if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
-            {
-                // Capslock On: disable blink, turn led on
-                blink_interval_ms = 0;
-                board_led_write(true);
-            }
-            else
-            {
-                // Caplocks Off: back to normal blink
-                board_led_write(false);
-                blink_interval_ms = BLINK_MOUNTED;
-            }
-        }
-    }
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-    static uint32_t start_ms = 0;
-    static bool led_state = false;
-
-    // blink is disabled
-    if (!blink_interval_ms)
-        return;
-
-    // Blink every interval ms
-    if (board_millis() - start_ms < blink_interval_ms)
-        return; // not enough time
-    start_ms += blink_interval_ms;
-
-    board_led_write(led_state);
-    led_state = 1 - led_state; // toggle
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)bufsize;
 }
